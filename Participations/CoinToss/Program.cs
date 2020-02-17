e5e9cd650c2a7dd75fc098f38e8e4a16b71db5d3
Peyton Kassing <PKass18@ou.edu>
2020-02-17 13:13:34 -0600

    meep

--- Participations/CoinToss/Program.cs
+++ Participations/CoinToss/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Peyton Kassing
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,42 @@ namespace CoinToss
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Heads or Tails? >>");
+            string guess = Console.ReadLine();
+            int userGuess = -1;
+            const int headsValue = 0;
+            const int tailsValue = 1;
+
+            if (guess == "Heads")
+            {
+                userGuess = headsValue;
+            }
+            else if (guess == "Tails")
+            {
+                userGuess = tailsValue;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid guess, try again!");
+            }
+
+            Random rand = new Random();
+            int randomNum = rand.Next(0, 2);
+
+            if (userGuess == randomNum)
+            {
+                Console.WriteLine("You Won!!");
+            }
+            else
+            {
+                Console.WriteLine("You Lost...");
+            }
+
+            const string name = "Peyton Kassing";
+                Console.WriteLine(name);
+
+            Console.ReadKey(); 
         }
     }
 }
---
+++ Participations/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static string msg = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+        static void Main(string[] args)
+        {
+            String lookupWord;
+            String replaceWord;
+            Console.WriteLine(msg);
+
+            Console.WriteLine("What word do you want to look for?");
+            lookupWord = Console.ReadLine();
+
+            Console.WriteLine("What do you want to change " + lookupWord + " to?");
+            replaceWord = Console.ReadLine();
+
+            bool wordPresent = msg.Contains(lookupWord);
+            
+            if (wordPresent == true)
+            {
+                Console.WriteLine(msg.Replace(lookupWord, replaceWord));
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I could not find your word, " + lookupWord + ".");
+                for (int i = lookupWord.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(lookupWord[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
