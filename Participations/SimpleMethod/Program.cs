1acfebba58c2f060b84fd2eb3d3a7068c78218c9
Peyton Kassing <PKass18@ou.edu>
2020-03-23 12:56:02 -0500

    simple method

---
+++ Participations/SimpleMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal;
+            string sound = "";
+
+            Console.WriteLine("Please enter a type of animal (cow, pig, chicken).");
+            animal = Console.ReadLine().ToLower();
+
+            sound = Speak(animal);
+            Console.WriteLine(sound);
+
+
+            Console.ReadKey();
+        }
+        static string Speak(string animal)
+        {
+            string sound = "";
+            if (animal == "cow")
+            {
+                sound = "Moo";
+               
+            }
+            else if (animal == "pig")
+            {
+                sound = "Oink";
+            }
+            else if (animal == "chicken")
+            {
+                sound = "Cockadoodaldoo";
+            }
+            return sound;
+        }
+    }
+}
