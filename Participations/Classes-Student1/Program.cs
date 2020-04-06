50cd7efedce5300a42b96ac83e0a1813424afeff
Peyton Kassing <PKass18@ou.edu>
2020-04-06 15:44:15 -0500

    Student

---
+++ Participations/Classes-Student1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Student1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first, second;
+
+            first = "Meep";
+            second = "meow";
+
+            Student student1 = new Student();
+            student1.FirstName = "Grant";
+            student1.LastName = "K";
+            student1.CourseNames.Add("MIS3013");
+            student1.CourseGrades.Add("A");
+            student1.CourseNames.Add("MIS3213");
+            student1.CourseGrades.Add("B");
+            student1.CourseNames.Add("MKT3323");
+            student1.CourseGrades.Add("C");
+            student1.CourseNames.Add("SCM3223");
+            student1.CourseGrades.Add("B");
+            student1.CourseNames.Add("SCM3523");
+            student1.CourseGrades.Add("A");
+
+            Student student2 = new Student(first, second);
+            student2.CourseNames.Add("MIS3013");
+            student2.CourseGrades.Add("C");
+            student1.CourseNames.Add("MIS3213");
+            student1.CourseGrades.Add("A");
+            student1.CourseNames.Add("MKT3323");
+            student1.CourseGrades.Add("C");
+            student1.CourseNames.Add("SCM3223");
+            student1.CourseGrades.Add("D");
+            student1.CourseNames.Add("SCM3523");
+            student1.CourseGrades.Add("F");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Classes-Student1/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Student1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<string> CourseNames { get; set; }
+
+        public List<string> CourseGrades { get; set; }
+
+        public Student()
+        {
+            FirstName = "Peyton";
+            LastName = "Kassing";
+            CourseNames = new List<string>();
+            CourseGrades = new List<string>();
+        }
+        public Student(string first, string second)
+        {
+            FirstName = first;
+            LastName = second;
+            CourseNames = new List<string>();
+            CourseGrades = new List<string>();
+        }
+
+        public double CalculateGPA()
+        {
+            double A = 4;
+            double B = 3;
+            double C = 2;
+            double D = 1;
+            double F = 0;
+
+            if(CourseGrades.Contains("A"))
+            {
+
+            }
+
+        }
+    }
+}
