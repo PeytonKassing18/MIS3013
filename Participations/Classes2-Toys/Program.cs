c1a774c49f2ea6a6539ed27ec303e9c684540929
Peyton Kassing <PKass18@ou.edu>
2020-04-08 15:40:49 -0500

    Toys completed

---
+++ Participations/Classes2-Toys/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2_Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toy toy1 = new Toy();
+            toy1.Manufacturer = "Nerf";
+            toy1.Name = "Dart Blaster";
+            toy1.Price = 19.99;
+            GetAisle(toy1);
+
+            Toy toy2 = new Toy();
+            toy2.Manufacturer = "Hot Wheels";
+            toy2.Name = "Race Car Track";
+            toy2.Price = 59.99;
+            GetAisle(toy2);
+
+            ToyBox box1 = new ToyBox();
+            box1.Toys.Add(toy1);
+
+            ToyBox box2 = new ToyBox();
+            box2.Toys.Add(toy2);
+
+            Console.ReadKey();
+        }
+        static string GetAisle(Toy t)
+        {
+            Random rand = new Random();
+            int r = rand.Next(1, 25);
+            string first = t.Manufacturer.Substring(0,1);
+            Console.WriteLine(first + r);
+            string Meep = Console.ReadLine();
+            return Meep;
+        }
+    }
+}
---
+++ Participations/Classes2-Toys/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2_Toys
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Aisle { get; set; }
+
+        public string ToyAisle()
+        {
+            return Aisle;
+        }
+
+        public Toy()
+        {
+            Manufacturer = "Lego";
+            Name = "Super Mario starter kit";
+            Price = 29.99;
+            Aisle = "Kids Toys";
+        }
+    }
+}
